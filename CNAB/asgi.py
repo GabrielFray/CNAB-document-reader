33b7d0ad93836eea98a888a1eea31b772db90a7f
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-25 17:12:12 -0300

    feat: Start Project

--- CNAB/asgi.py
+++ CNAB/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CNAB project.
+ASGI config for cnab project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CNAB.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cnab.settings")
 
 application = get_asgi_application()
