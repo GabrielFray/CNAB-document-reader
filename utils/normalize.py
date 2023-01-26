e5431eb2b3e7544d55d784258d5057846be41cd3
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2023-01-26 17:54:51 -0300

    feat: Normalize cnab file uploaded

---
+++ utils/normalize.py
@@ -0,0 +1,20 @@
+from cnab_transactions.models import CnabTransactions
+
+
+def normalize_file_txt(file) -> dict:
+    read_file = file.read().decode("utf-8")
+    format_file = read_file.split("\r\n")
+
+    for line in format_file:
+        cnab_data = {
+            "type": line[0],
+            "date": f"{line[1:5]}-{line[5:7]}-{line[7:9]}",
+            "value": round(int(line[9:19]) / 100, 2),
+            "cpf": line[19:30],
+            "card": line[30:42],
+            "hour": f"{line[1:5]}-{line[5:7]}-{line[7:9]} {line[42:44]}:{line[44:46]}:{line[46:48]}",
+            "owner": line[48:62].strip(),
+            "store": line[62:].strip(),
+        }
+
+        CnabTransactions.objects.create(**cnab_data)
